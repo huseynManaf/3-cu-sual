0306dba25531d613b338d256356a5007c76d6aaf
huseynManaf <huseynim@code.edu.az>
2022-12-28 04:23:21 -0800

    init

---
+++ 2 ci suall/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _2_ci_suall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            if (n > 9999 && n < 100000)
+            {
+                Console.Write($"{4}{n}{2}");
+            }
+            else
+            {
+                Console.WriteLine("5 reqemli eded deyil");
+
+            }
+        }    
+    }
+}
